23e2f5bcbb1dbab64b90766b45ea4f94d4b3dec8
Satish Samad <wnsatish@gmail.com>
2026-04-30 10:03:16 +0530

    Added Delete Alert Dialog

---
+++ app/src/main/java/com/devsatish/produck/ui/screen/components/DeleteAlertDialog.kt
@@ -0,0 +1,90 @@
+package com.devsatish.produck.ui.screen.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun DeleteAlertDialog(
+    showDialog: Boolean,
+    title: String,
+    onDelete: () -> Unit,
+    onCancel: () -> Unit
+) {
+    if (showDialog) {
+        Dialog(onDismissRequest = { onCancel() }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White, shape = RoundedCornerShape(16.dp))
+                    .padding(horizontal = 14.dp, vertical = 12.dp)
+            ) {
+                Text(
+                    text = "Alert Dialog",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(Modifier.height(8.dp))
+
+                Text(
+                    text = "Are you sure want to delete \"$title\"?",
+                    fontSize = 15.sp
+                )
+                Spacer(Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        onClick = { onDelete() },
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Red,
+                            contentColor = Color.White
+                        ),
+                        border = BorderStroke(1.dp, Color.White),
+                        elevation = ButtonDefaults.buttonElevation(
+                            defaultElevation = 6.dp
+                        )
+                    ) {
+                        Text("Delete")
+                    }
+
+                    Button(
+                        onClick = { onCancel() },
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black
+                        ),
+                        border = BorderStroke(1.dp, Color.Gray),
+                        elevation = ButtonDefaults.buttonElevation(
+                            defaultElevation = 6.dp
+                        )
+                    ) {
+                        Text("Cancel")
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/devsatish/produck/ui/theme/Fonts.kt
@@ -0,0 +1,17 @@
+package com.devsatish.produck.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.devsatish.produck.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_medium)
+)
+
+val TitleGreet = FontFamily(
+    Font(R.font.fellgreet)
+)
+
+val TitleGreet2 = FontFamily(
+    Font(R.font.jacquesfrancoisregular)
+)
